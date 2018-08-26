2271e7ead50ce1e30191c821bb484dff656ddca5
Fokin Alex <elgokbackground@yahoo.com>
2018-08-26 16:07:57 +0300

    schema.sql splited -> schema.sql & data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+-- Document table insertion
+insert into document values(3, 'Birth certificate');
+insert into document values(7, 'Military card');
+insert into document values(8, 'Temporary certificate (military card)');
+insert into document values(10, 'Foreign citizen passport');
+insert into document values(11, 'Refugee petition consideration certificate');
+insert into document values(12, 'Residence permit');
+insert into document values(13, 'Refugee certificate');
+insert into document values(15, 'Temporary residence permission');
+insert into document values(18, 'Temporary refuge certificate');
+insert into document values(21, 'Russian Federation passport');
+insert into document values(23, 'Another country birth certificate');
+insert into document values(24, 'Russian Federation serviceman`s identity card');
+insert into document values(91, 'Other documents');
+
+-- Citizenship table insertion
+insert into citizenship values(643, 'Russian Federation');
+insert into citizenship values(674, 'San Marino');
+insert into citizenship values(392, 'Japan');
+insert into citizenship values(840, 'United States of America');
+insert into citizenship values(826, 'United Kingdom');
